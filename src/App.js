72e96d1c7734c075373e9505cf0bff53478a1bb0
Marvin Abarca <marvinabarca0@gmail.com>
2023-10-08 23:24:55 -0500

    quick edit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello Max! i love you dad</p>
+        <p>I love you dad, from Max</p>
+        <p>test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
